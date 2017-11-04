450fc4f2e4aff2f1ea84c69312cfbb6145d2ce16
root <root>
2017-11-04 09:05:43 +0800

    2017yii第二次更新

--- frontend/views/brand/add.php
+++ frontend/views/brand/add.php
@@ -5,7 +5,7 @@ echo $form->field($brands,'intro');
 echo $form->field($brands,'sort');
 echo $form->field($brands,'status')->inline()->radioList(\app\models\Brand::$statusText);
 echo $form->field($brands,'imgFile')->fileInput();
-echo \yii\bootstrap\Html::img('@web/'.$brand->logo,['height'=>40]);
+echo \yii\bootstrap\Html::img('@web/'.$brands->logo,['height'=>40]);
 
 echo \yii\bootstrap\Html::submitButton("提交",['class'=>'btn btn-success']);
 \yii\bootstrap\ActiveForm::end();
